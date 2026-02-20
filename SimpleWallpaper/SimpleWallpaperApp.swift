fa036ee97f8ab737e42720b5215ae8c7119909ee
tangxiao <664620323@qq.com>
2026-02-20 15:40:33 +0800

    Initial Commit

---
+++ SimpleWallpaper/SimpleWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWallpaperApp.swift
+//  SimpleWallpaper
+//
+//  Created by 唐潇 on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleWallpaperTests/SimpleWallpaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWallpaperTests.swift
+//  SimpleWallpaperTests
+//
+//  Created by 唐潇 on 2026/2/20.
+//
+
+import Testing
+@testable import SimpleWallpaper
+
+struct SimpleWallpaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
